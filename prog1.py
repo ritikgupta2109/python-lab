02e96b12a2a7bfda776c22c45b327624115c10cb
ritikgupta2109 <60683721+ritikgupta2109@users.noreply.github.com>
2020-02-12 10:49:33 +0530

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,8 @@
+l =list(map(int,input().split()))
+b=[]
+for i in l:
+                    if i not in b:
+                                        b.append (i)
+for i in b:
+      print(i,end=" ")                                        
+                                        
