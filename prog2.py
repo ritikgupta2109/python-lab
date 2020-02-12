53698d9d57e7892e638eaf930aa7e1833c648974
ritikgupta2109 <60683721+ritikgupta2109@users.noreply.github.com>
2020-02-12 11:37:05 +0530

    Add files via upload

---
+++ prog2.py
@@ -0,0 +1,13 @@
+for i in range(1,101):
+                    if(i%3==0 and i%5==0):
+                                        print("**")
+                    elif(i%3==0):
+                                        print("#")
+                    elif(i%5==0):
+                                        print("@")
+                    else:
+                                        print(i)                    
+                                                                                             
+                                        
+                                 
+                                        
